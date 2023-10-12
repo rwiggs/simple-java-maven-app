bcee965ffa0cbd03a7d9d723e98fd66eea4bd0fb
rwiggs <robin.wiggs@gmail.com>
2023-10-12 14:08:10 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -9,11 +9,11 @@ mvn jar:jar install:install help:evaluate -Dexpression=project.name
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]" | sed 's/\n//g'`
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]" | sed 's/\n//g`
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
