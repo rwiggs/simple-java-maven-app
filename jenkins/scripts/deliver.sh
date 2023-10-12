aeed63c0908addebba6682b3e479927bb01d6a29
rwiggs <robin.wiggs@gmail.com>
2023-10-12 14:17:03 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -19,6 +19,4 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 MYAPP="$(echo ${NAME}-${VERSION}.jar | tr -dc '\n\r'  )"
 
-pwd
-ls target/my-app-1.0-SNAPSHOT.jar
 java -jar target/${MYAPP}
