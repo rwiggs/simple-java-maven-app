b8b9d0b4820237f1942244974a2cc410364d401b
rwiggs <robin.wiggs@gmail.com>
2023-10-12 14:15:14 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -17,7 +17,7 @@ VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]" | sed 's
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-MYAPP="$(echo ${NAME}-${VERSION}.jar | tr -dc '[:alnum:]\n\r' | sed 's/0m/-/g' )"
+MYAPP="$(echo ${NAME}-${VERSION}.jar | tr -dc '\n\r'  )"
 
 pwd
 ls target/my-app-1.0-SNAPSHOT.jar
